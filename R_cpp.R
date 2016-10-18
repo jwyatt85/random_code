b3e72fa02d1df288f59d519d0befd901e397d50d
jwyatt85 <jimmywyatt@gmail.com>
2016-10-18 11:04:33 -0400

    updated cpp file

--- R_cpp.R
+++ R_cpp.R
@@ -1,20 +1,29 @@
 
-
+#### Load sources ####
 library(Rcpp)
 library(microbenchmark)
+library(rbenchmark)
 
-cppFunction('int add(int x, int y, int z) {
-  int sum = x + y + z;
-            return sum;
-            }')
+url <- "https://raw.githubusercontent.com/jwyatt85/random_code/master/sources/export.cpp"
+destfile <- paste0(tempdir(), "/mysource.cpp")
+download.file(url, destfile, mode="wb")
+sourceCpp(destfile)
 
-add(1,2,3)
-
-sourceCpp("~/Desktop/learning_cpp_r/export.cpp")
-
-convolveCpp(3,10)
-
+### Benchmarking ####
 microbenchmark(
   mean(c(2,3,4,5,6)),
   james(2)
 )
+
+a <- replicate(250, 1:100, simplify=FALSE)
+
+res <- benchmark(as.data.frame(a), 
+                 CheapDataFrameBuilder(a), 
+                 order="relative", replications=500)
+
+microbenchmark(
+  as.data.frame(a), 
+  CheapDataFrameBuilder(a)
+)
+
+a[1]
