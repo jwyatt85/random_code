bdeca5a30b4807ce7cfa6d5dfe61ed4d9bee3acc
jwyatt85 <jimmywyatt@gmail.com>
2016-11-22 12:07:20 -0500

    updates
    updated printing and added new function calls

--- sheets.R
+++ sheets.R
@@ -9,7 +9,7 @@ args <- commandArgs(trailingOnly = TRUE)
 suppressPackageStartupMessages({
   library("dplyr")
   library("googlesheets")
-  library("knitr")
+  suppressWarnings(library("knitr"))
 })
 
 check_clients <- function(call){
@@ -17,8 +17,9 @@ check_clients <- function(call){
   gap <- suppressMessages(googlesheets::gs_title("Client_list"))
   clients1 <- suppressMessages(googlesheets::gs_read(gap, ws=1))
   clients2 <- suppressMessages(googlesheets::gs_read(gap, ws=2))
+  clients3 <- suppressMessages(googlesheets::gs_read(gap, ws=3))
   
-  if(call == "all"){
+  if(call == "all") {
     clients1 <-clients1 %>% 
       mutate(
         last_contact = as.Date(last_contact, "%m/%d/%Y"),
@@ -32,14 +33,38 @@ check_clients <- function(call){
     clients2 <- clients2 %>% 
       arrange(desc(poll))
     
+    clients3 <- clients3 %>% 
+      mutate(
+        `due date` =  as.Date(`due date`, "%m/%d/%Y")
+      ) %>% 
+      filter(!is.na(active)) %>% 
+      arrange(`due date`)
+    
     print(kable(clients1)) #output to terminal
-    print(kable(clients2))  #output to terminal
-   
-  } else
-    print("give me a call")
+    print(kable(clients2)) #output to terminal
+    print(kable(clients3))
+    cat("\n\n\n\n")
+    
+  } else if(call == "edit") {
+      browseURL("https://docs.google.com/spreadsheets/d/1W3SnvZqTlZ1etcfbVpigca1IROFknm3gyoSxGuDmyXM/edit#gid=0", 
+                browser = getOption("browser"),
+                encodeIfNeeded = FALSE)
+  } else if(call == 'todo') {
+    clients3 <- clients3 %>% 
+      mutate(
+        `due date` =  as.Date(`due date`, "%m/%d/%Y")
+      ) %>% 
+      filter(!is.na(active)) %>% 
+      arrange(`due date`)
+    
+      print(kable(clients3))
+      cat("\n\n\n")
+    
+    } else {
+    stop("call me please")
+  }
 }
 
 check_clients(args)
-
 
 
