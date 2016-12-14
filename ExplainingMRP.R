0838f6b92c1733a8be54564fa9e1ae87bef19421
jwyatt85 <jimmywyatt@gmail.com>
2016-12-14 11:43:19 -0500

    Added the ability to create fake data for all 50 states

--- ExplainingMRP.R
+++ ExplainingMRP.R
@@ -4,32 +4,41 @@ require(purrr, quietly = T)
 require(dplyr, quietly = T)
 require(blme, quietly = T)
 
+set.seed(12345)
+
 ### Section: Setting up Data Sets ####
-state_FL <- list(
-  age = c("18-29", "30-44", "45-64", "65+"),
-  gender = c("Male", "Female"),
-  race = c("white", "non-white")
-)
+state_loop <- c(state.abb[1:9], "DC", state.abb[10:length(state.abb)])
+state_list <- lapply(state_loop, function(i){
+  list(
+    age = c("18-29", "30-44", "45-64", "65+"),
+    gender = c("Male", "Female"),
+    race = c("white", "non-white"),
+    state = i
+  )
+})
 
-state_stratas <- state_FL %>%
-  cross_d() %>% 
-  data.frame() %>% 
-  mutate(
-    state = "Florida",
-    Obama12 = .5120,
-    unemployment = .08,
-    percent_pop = 1/NROW(.)
-  ) %>% 
-  map_if(is.factor, as.character) %>% 
-  tbl_df()
+names(state_list) <- state_loop
 
-state_grouping_vars <- list(
-  state = c("Florida", "Ohio", "Georgia"),
-  Obama12 = c(.5120, .5023, .45949),
-  unemployment = c(.08, .10, .09)
-) %>% 
-  bind_rows() %>% 
-  tbl_df
+state_list <- state_list %>% 
+  map(function(x){
+    x %>% 
+      cross_d() %>% 
+      data.frame() %>% 
+      mutate(
+        Obama12 = runif(1, .30, .80),
+        unemployment = runif(1, .05, .10),
+        percent_pop = 1/NROW(.)
+      )
+  })
+
+
+state_grouping_vars <- state_list %>% 
+  map_df(function(x){
+    x %>% 
+      select(state, Obama12, unemployment) %>% 
+      unique()
+  })
+
 
 suppressWarnings(
   survey_df <- data.frame(
@@ -37,7 +46,7 @@ suppressWarnings(
     gender = replicate(10000, sample(c("Male", "Female"), prob = c(.48, .52), size = 1)),
     race = replicate(10000, sample(c("white", "non-white"), prob = c(.80, .20), size = 1)),
     dependent_variable = replicate(10000, sample(c(1, 0), prob = c(.60, .40), size = 1)),
-    state = as.character(replicate(10000, sample(c("Florida", "Ohio", "Georgia"), prob = c(.60, .20, .20), size = 1)))
+    state = as.character(replicate(10000, sample(state_loop, size = 1)))
   ) %>% 
     left_join(state_grouping_vars, by = "state") %>% 
     map_if(is.factor, as.character) %>% 
@@ -47,23 +56,21 @@ suppressWarnings(
 #### Section: Multilevel Modeling ####
 
 fm1 <- suppressWarnings(blmer(dependent_variable ~ (1|age) 
-             + (1|gender) 
-             + (1|race)
-             + Obama12
-             + unemployment, data = survey_df))
+                              + (1|gender) 
+                              + (1|race)
+                              + Obama12
+                              + unemployment, data = survey_df))
 
-# survey_df$predict <- predict(fm1, type="response")
-
-state_stratas$predicted <- predict(fm1, newdata = state_stratas, type = "response")
-
-state_stratas %>% 
-  mutate(
-    sum_me = percent_pop * predicted
-  ) %>% 
-  summarise(FL_MRP = sum(sum_me)) %>% 
-  map_dbl(as.numeric)
-  
-
-
+### Predict and summarise ####
+state_pred <- state_list %>% 
+  map(function(x){
+    x %>% 
+      mutate(
+        strat_pred = predict(fm1, newdata = ., type = "response"),
+        state_pred = strat_pred * percent_pop
+      ) %>% 
+      summarise(state_total = sum(state_pred))
+  }) %>% 
+  bind_rows(.id = "state") 
 
 
