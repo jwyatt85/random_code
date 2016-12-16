e60ff2b251b60591b530c90eae0ef3bd9d791949
jwyatt85 <jimmywyatt@gmail.com>
2016-12-16 12:08:07 -0500

    added geo-coding too

--- best_mapping.R
+++ best_mapping.R
@@ -12,4 +12,24 @@ leaflet() %>%
 library(maps)
 mapStates = map("state", fill = TRUE, plot = FALSE)
 leaflet(data = mapStates) %>% addTiles() %>%
-  addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
+  addPolygons(fillColor = topo.colors(5, alpha = .5), stroke = F)
+
+?addPolygons()
+?topo.colors()
+
+### Get lat/long from Address ####
+geocodeAdddress <- function(address) {
+  require(RJSONIO)
+  url <- "http://maps.google.com/maps/api/geocode/json?address="
+  url <- URLencode(paste(url, address, "&sensor=false", sep = ""))
+  x <- fromJSON(url, simplify = FALSE)
+  if (x$status == "OK") {
+    out <- c(x$results[[1]]$geometry$location$lng,
+             x$results[[1]]$geometry$location$lat)
+  } else {
+    out <- NA
+  }
+  Sys.sleep(0.2)  # API only allows 5 requests per second
+  out
+}
+
