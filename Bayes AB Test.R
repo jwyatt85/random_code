98cc274e9d80372ca9b88c660f08fa0275c3da2f
jwyatt85 <jimmywyatt@gmail.com>
2017-01-10 16:08:46 -0500

    transitioned to ggplot2

--- Bayes AB Test.R
+++ Bayes AB Test.R
@@ -6,9 +6,19 @@ library(tidyr)
 
 curve(dbeta(x, 1,1)) # these are our weak priors for the AB test.  We don't know or have any reason to believe A over B
 
-curve(dbeta(x, 36+4, 114+4), col = "blue")
-curve(dbeta(x, 50+4, 100+4), add = TRUE, col = "red")
- 
+# # curve(dbeta(x, 36+4, 114+4), col = "blue")
+# # curve(dbeta(x, 50+4, 100+4), add = TRUE, col = "red")
+
+x <- seq(0,1,length=100)
+db <- dbeta(x, 36+4, 114+4)
+db2 <- dbeta(x, 50+4, 100+4)
+
+ggplot() + geom_line(aes(x, db), colour = "blue", linetype = 1, size = 1) + 
+  geom_line(aes(x, db2), colour = "red", linetype = 1, size = 1) + theme_bw()
+
+# ggplot() + geom_line(aes(x, db), colour = "blue") + 
+#   geom_line(aes(x, db2), colour = "red")
+
 n.trials <- 100000
 prior.alpha <- 4
 prior.beta <- 4
