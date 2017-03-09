0096cef8b51256a1157c68ca5be47bb082eb2c65
jwyatt85 <jimmywyatt@gmail.com>
2017-03-09 00:02:10 -0500

    added 2014 data

--- Miami_Dade_Voter_Reg_Data.R
+++ Miami_Dade_Voter_Reg_Data.R
@@ -21,9 +21,36 @@ for(i in 1:12){
   cat("downloaded file # ", i)
 }
 
+### Links for 2014 data ####
+# looks like in July the switched to the new link version
+for(i in 1:12){
+  if(i >=7){
+    if(i < 10){
+      download.file(
+        paste0('http://www.miamidade.gov/elections/STATS/2014/2014-0',i,'-voter-registration-statistics-districts.xls'),
+        destfile = paste0('MD_file_2014_',i)
+      )
+    } else {
+      download.file(
+        paste0('http://www.miamidade.gov/elections/STATS/2014/2014-',i,'-voter-registration-statistics-districts.xls'),
+        destfile = paste0('MD_file_2014_',i)
+      )
+    }
+  } else {
+    download.file(
+      paste0('https://www.miamidade.gov/elections/STATS/2014/', tolower(month.abb)[i], '14dist.xls'),
+      destfile = paste0('MD_file_2014_',i)
+    )
+  }
+  cat("downloaded file # ", i, "\n")
+}  
+
+
+### Save it all ####
 temp <- list.files("~/Desktop/MD_files/")
 setwd("~/Desktop/MD_files/")
 
+### Mutates ####
 myfiles <- lapply(temp, function(i){
   year <- unique(na.omit(as.numeric(unlist(strsplit(unlist(i), "[^0-9]+")))))[1]
   month_num <- unique(na.omit(as.numeric(unlist(strsplit(unlist(i), "[^0-9]+")))))[2]
@@ -84,8 +111,8 @@ final_MD_reg_stats <- myfiles %>%
 # readr::write_rds(final_MD_reg_stats, '~/Desktop/MD_files/final_MD_reg_stats.rds')
 df_list <- readr::read_rds("~/Desktop/MD_files/final_MD_reg_stats.rds")
 
-df_list[[1]]$`116th House District` %>%
-  filter(demographic == '**TOTAL**')
+# df_list[[1]]$`116th House District` %>%
+#   filter(demographic == '**TOTAL**')
 
 totals <- lapply(1:length(df_list), function(i){
   df_list[[i]]$`116th House District`
@@ -100,9 +127,9 @@ totals_final <- totals[order(totals$date),, drop = F] %>%
   select(date, percent_dems, percent_reps, percent_npa, demographic) %>% 
   reshape2::melt(., id = c("date", "demographic"))
 
-test <- ggplot(totals_final, aes(x=date, y=value, color = variable)) +
-  theme_bw() + facet_grid(variable ~ demographic) + geom_line()
-test 
+# test <- ggplot(totals_final, aes(x=date, y=value, color = variable)) +
+#   theme_bw() + facet_grid(variable ~ demographic) + geom_line()
+# test 
 
 test2 <- ggplot(totals_final, aes(x=date, y=value, color = variable)) +
   theme_bw() + facet_grid(. ~ demographic) + geom_line()
@@ -127,6 +154,8 @@ totals_final <- hispanic_trends[order(hispanic_trends$date),, drop = F] %>%
 test <- ggplot(totals_final, aes(x=date, y=value, color = variable)) +
   theme_bw() + geom_line() + facet_grid(. ~ demographic)
 test 
+
+
 
 
 
